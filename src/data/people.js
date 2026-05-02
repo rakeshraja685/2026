daba8ed266f5dcc03157ff228e9ec51451ba2a03
rakeshraja685 <rakeshraja685@gmail.com>
2026-05-02 21:16:57 +0530

    Add new people to Yearbook from People directory

--- src/data/people.js
+++ src/data/people.js
@@ -8,6 +8,36 @@ export const people = [
     quote: "I built this website to preserve our beautiful memories. Keep moving forward, Class of 2026! It's not a goodbye, just a 'see you later'.",
     funFact: "Created this entire class website from scratch to ensure our legacy lives on.",
     category: "Innovators",
+  },
+  {
+    id: 2,
+    name: "MST.JAYANTH.V",
+    nickname: "Jayanth",
+    photo: "/images/people/MST.JAYANTH.V.jpg",
+    superlative: "Most Likely to Succeed",
+    quote: "Making memories everywhere.",
+    funFact: "Always knows the right thing to say.",
+    category: "Legacy Leaders",
+  },
+  {
+    id: 3,
+    name: "RAGHAVA",
+    nickname: "Raghava",
+    photo: "/images/people/RAGHAVA.png",
+    superlative: "Life of the Party",
+    quote: "A journey of a thousand miles begins with a single step.",
+    funFact: "Always bringing great energy to the group.",
+    category: "Arts & Culture",
+  },
+  {
+    id: 4,
+    name: "Ahamathlla",
+    nickname: "Ahamathlla",
+    photo: "https://ui-avatars.com/api/?name=Ahamathlla&background=random&size=512",
+    superlative: "Quiet Achiever",
+    quote: "Still waters run deep.",
+    funFact: "Has a hidden talent we are yet to discover.",
+    category: "Innovators",
   }
 ];
 
